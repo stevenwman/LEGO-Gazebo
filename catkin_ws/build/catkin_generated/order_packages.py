61b5536890aeacd52211d883e8e914e81682f77c
stevenwman <sman2@andrew.cmu.edu>
2024-03-06 21:16:10 -0800

    making my own package

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/sman/Work/CMU/Research/LEGO-project/LEGO-Gazebo/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/sman/Work/CMU/Research/LEGO-project/LEGO-Gazebo/Test/devel;/opt/ros/noetic'.split(';') if '/home/sman/Work/CMU/Research/LEGO-project/LEGO-Gazebo/Test/devel;/opt/ros/noetic' != '' else []
